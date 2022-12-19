92d6cde00eeb176e32c9bd452c8d14b5d308d77a
AndrewTrackim <andrewttrackim@gmail.com>
2022-12-19 13:49:05 -0500

    merged graphs and added additional info

--- process.py
+++ process.py
@@ -1,12 +1,13 @@
 f = open("tides.csv", 'r')
 height = []
 time = []
+midnight_passed = False
 for x in f:
     parts = x.split(',')
     print(parts)
     print(parts[1][0:len(parts[1]) - 2])
     height.append(float(parts[1][0:len(parts[1]) - 2]))
-    time.append(parts[0])
+    time.append('2022.12.19 ' + parts[0] + ":00")
 
 print(height)
 print(time)
